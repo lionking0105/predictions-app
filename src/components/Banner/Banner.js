94c94151a693e0b9f37b0382eadf925f587f433e
lionking0105 <alexanderfoster0105@gmail.com>
2023-05-10 07:54:53 -0400

    add on click install pwa

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import adImg from "../../assets/ad.jpg";
 const Banner = () => {
-  return <img src={adImg} alt="banner" className="rounded cursor-pointer" />;
+  const [supportsPWA, setSupportsPWA] = useState(false);
+  const [promptInstall, setPromptInstall] = useState(null);
+  useEffect(() => {
+    const handler = (e) => {
+      e.preventDefault();
+      console.log("we are being triggered :D");
+      setSupportsPWA(true);
+      setPromptInstall(e);
+    };
+    window.addEventListener("beforeinstallprompt", handler);
+
+    return () => window.removeEventListener("transitionend", handler);
+  }, []);
+  const onClick = (evt) => {
+    evt.preventDefault();
+    if (!promptInstall) {
+      return;
+    }
+    promptInstall.prompt();
+  };
+  return (
+    <img
+      src={adImg}
+      alt="banner"
+      className="rounded cursor-pointer"
+      onClick={onClick}
+    />
+  );
 };
 
 export default Banner;
