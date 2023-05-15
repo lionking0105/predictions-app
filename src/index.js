8bcf32276b6ff0824b45048abdc5e21982bbb21d
lionking0105 <alexanderfoster0105@gmail.com>
2023-05-15 04:10:17 -0400

    persist data after refresh

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
-import { store } from "./store";
+import { store, persistor } from "./store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store.js
+++ src/store.js
@@ -1,9 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userSlice from "./features/user/userSlice";
 import gameSlice from "./features/game/gameSlice";
-export const store = configureStore({
-  reducer: {
-    user: userSlice,
-    game: gameSlice,
-  },
+
+// Setup redux-persist configuration
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["game"], // assuming 'game' is where 'selectedGame' is stored
+};
+
+// Create a root reducer
+const rootReducer = combineReducers({
+  user: userSlice,
+  game: gameSlice,
 });
+
+// Use persistReducer to create a persisted version of the root reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
