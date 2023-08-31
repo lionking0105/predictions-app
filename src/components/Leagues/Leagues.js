e3beb33162aeef80d7b94d194b5d0a7c16b4cab2
lionking0105 <alexanderfoster0105@gmail.com>
2023-08-31 17:34:15 -0400

    save selected league to LS

--- src/components/Leagues/Leagues.js
+++ src/components/Leagues/Leagues.js
@@ -1,40 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectLeague } from "../../features/game/gameSlice";
 import { showMobileLeague } from "../../features/user/userSlice";
 import leagues from "../../utils/leagues";
 const Leagues = ({ isMobile }) => {
-  const dispatch = useDispatch();
-  const { selectedLeague } = useSelector((store) => store.game);
-  const handleLeagueClick = (id, leagueName, country) => {
-    dispatch(selectLeague({ id, leagueName, country }));
-    if (isMobile) {
-      dispatch(showMobileLeague());
-    }
-  };
-  return (
-    <section className="leagues w-full rounded p-5 dark-bg h-fit lg:w-3/12">
-      <h1 className="mb-5 text-xl text-white ">Leagues</h1>
-      <ul>
-        {leagues.map((league) => {
-          const { id, leagueName, path, countryName } = league;
-          const active = id === selectedLeague.id ? "active" : "";
-          return (
-            <li
-              key={id}
-              className={`flex cursor-pointer pb-4 last:pb-0 hover:translate-x-1 transition-all duration-300 ${active}`}
-              onClick={() => {
-                handleLeagueClick(id, leagueName, countryName);
-              }}
-            >
-              <img src={path} alt={leagueName} className="w-6 mr-2" />
-              {leagueName}
-            </li>
-          );
-        })}
-      </ul>
-    </section>
-  );
+    const dispatch = useDispatch();
+    const { selectedLeague } = useSelector((store) => store.game);
+    const [currentLeague, setCurrentLeague] = useState();
+
+    useEffect(() => {
+        const savedLeague = JSON.parse(localStorage.getItem("selectedGame"));
+        if (savedLeague) {
+            dispatch(selectLeague(savedLeague));
+        }
+
+        if (currentLeague) {
+            localStorage.setItem("selectedGame", JSON.stringify(currentLeague));
+            dispatch(selectLeague(currentLeague));
+        }
+    }, [currentLeague, dispatch]);
+
+    return (
+        <section className="leagues w-full rounded p-5 dark-bg h-fit lg:w-3/12">
+            <h1 className="mb-5 text-xl text-white ">Leagues</h1>
+            <ul>
+                {leagues.map((league) => {
+                    const {
+                        id,
+                        leagueName,
+                        path,
+                        countryName: country,
+                    } = league;
+                    const active = id === selectedLeague.id ? "active" : "";
+                    return (
+                        <li
+                            key={id}
+                            className={`flex cursor-pointer pb-4 last:pb-0 hover:translate-x-1 transition-all duration-300 ${active}`}
+                            onClick={() => {
+                                setCurrentLeague({
+                                    id,
+                                    leagueName,
+                                    country,
+                                });
+                            }}
+                        >
+                            <img
+                                src={path}
+                                alt={leagueName}
+                                className="w-6 mr-2"
+                            />
+                            {leagueName}
+                        </li>
+                    );
+                })}
+            </ul>
+        </section>
+    );
 };
 
 export default Leagues;
